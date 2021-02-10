7927ce5b243bdb0ef370039ee7323a07f869d864
ayankhann98@gmail.com <ayankhann98@gmail.com>
2021-02-10 10:53:23 +0530

    First custom listView app

---
+++ App2/Class1.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App2
+{
+    class ListViewAdapter : BaseAdapter<string>
+    {
+        List<string> list;
+        Context context;
+        public ListViewAdapter(Context context, List<string> list)
+        {
+            this.context = context;
+            this.list = list;
+        }
+        public override string this[int position]
+        {
+            get { return list[position]; }
+        }
+
+
+        public override int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            if(row==null)
+            {
+                row = LayoutInflater.From(context).Inflate(Resource.Layout.List_view_layout, null, false);
+            }
+            TextView text = row.FindViewById<TextView>(Resource.Id.textName);
+            text.Text = list[position];
+
+            return row;
+
+        }
+    }
+}
---
+++ App2/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace App2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int count = 1;
+        private List<String> lists = new List<string>();
+        private ListView view;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            view = FindViewById<ListView>(Resource.Id.listView1);
+            lists.Add("Ayan");
+            lists.Add("is");
+            lists.Add("the");
+            lists.Add("Boss");
+
+            ListViewAdapter adapter = new ListViewAdapter(this, lists);
+            view.Adapter = adapter;
+        }
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+    }
+}
